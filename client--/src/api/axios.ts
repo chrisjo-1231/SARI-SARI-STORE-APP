adb02b618ee90f92f7705ab0a3d9d0a85ffead0c
chrisjo-1231 <christianjosephrubis.1231@gmail.com>
2026-07-24 21:21:09 +0800

    update backend api url

--- client--/src/api/axios.ts
+++ client--/src/api/axios.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
-    baseURL:"http://localhost:5000/api"
-
+    baseURL: "https://sari-sari-store-app.onrender.com/api"
 });
-
 
 export default api;
