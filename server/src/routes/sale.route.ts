af4d4dc6c0abd7c34a8f71369415085dfd4762cf
chrisjo-1231 <167882628+chrisjo-1231@users.noreply.github.com>
2026-07-25 11:34:34 +0800

    Update sale.route.ts

--- server/src/routes/sale.route.ts
+++ server/src/routes/sale.route.ts
@@ -2,20 +2,21 @@ import { Router } from "express";
 import { 
 storeSale,
 indexSales,
-showSale
+showSale,
+receiptSale
 } from "../controllers/sale.controller.js";
 
 
 const router = Router();
 
 
-router.post("/",storeSale);
+router.post("/", storeSale);
 
+router.get("/", indexSales);
 
-router.get("/",indexSales);
+router.get("/:id", showSale);
 
-
-router.get("/:id",showSale);
+router.get("/receipt/:id", receiptSale);
 
 
 export default router;
